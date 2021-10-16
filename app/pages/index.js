8f7bb39d4491dfeca9b5057e356b3e88e521fa58
AR <74219764+AR-Student824@users.noreply.github.com>
2021-10-16 14:29:27 -0700

    ;

--- app/pages/index.js
+++ app/pages/index.js
@@ -21,12 +21,12 @@ export default function Home() {
 
         <div className="frcss_cardGrid">
           <a href="https://nextjs.org/docs" className="frcss_card">
-            <h2>Documentation &rarr;</h2>
+            <h2>Documentation</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="frcss_card">
-            <h2>Learn &rarr;</h2>
+            <h2>Learn</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -34,7 +34,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="frcss_card"
           >
-            <h2>Examples &rarr;</h2>
+            <h2>Examples</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -42,7 +42,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="frcss_card"
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Deploy</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
